e3ed0e0e8ac48e69d7026834be75d230c2a7c78c
Alister McKinley <alpatine@gmail.com>
2020-07-13 21:32:55 +1000

    Set up unit tests for p1

--- p1_test.py
+++ p1_test.py
@@ -2,8 +2,8 @@ import unittest
 from p1 import p1
 
 class P1Test(unittest.TestCase):
-    def test_10(self):
+    def test_p1_10(self):
         self.assertEqual(p1(10), 23)
     
-    def test_1000(self):
+    def test_p1_1000(self):
         self.assertEqual(p1(1000), 233168)
