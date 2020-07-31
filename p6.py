5233f19650582009fea512776aee698fd6a7204e
Alister McKinley <alpatine@gmail.com>
2020-07-31 22:28:16 +1000

    Implement problem 6

---
+++ p6.py
@@ -0,0 +1,8 @@
+from sums import sum_numbers, sum_square_numbers
+
+def p6(upper):
+    return sum_numbers(upper) ** 2 - sum_square_numbers(upper)
+
+if __name__ == '__main__':
+    print(p6(11))
+    print(p6(101))
---
+++ p6_test.py
@@ -0,0 +1,9 @@
+import unittest
+from p6 import p6
+
+class P6Test(unittest.TestCase):
+    def test_p6_11(self):
+        self.assertEqual(p6(11), 2640)
+        
+    def test_p6_101(self):
+        self.assertEqual(p6(101), 25164150)
