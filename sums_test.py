9d6627ca873b54655777e1e215411ccab3605425
Alister McKinley <alpatine@gmail.com>
2020-07-31 22:28:09 +1000

    Implement integer and square sums

---
+++ sums_test.py
@@ -0,0 +1,28 @@
+import unittest
+from sums import sum_numbers, sum_square_numbers
+
+class Sum_Numbers_Test(unittest.TestCase):
+    def test_sum_numbers_1(self):
+        self.assertEqual(sum_numbers(1), 0)
+    
+    def test_sum_numbers_2(self):
+        self.assertEqual(sum_numbers(2), 1)
+    
+    def test_sum_numbers_3(self):
+        self.assertEqual(sum_numbers(3), 3)
+
+    def test_sum_numbers_11(self):
+        self.assertEqual(sum_numbers(11), 55)
+
+class Sum_Square_Numbers_Test(unittest.TestCase):
+    def test_sum_square_numbers_1(self):
+        self.assertEqual(sum_square_numbers(1), 0)
+    
+    def test_sum_square_numbers_2(self):
+        self.assertEqual(sum_square_numbers(2), 1)
+    
+    def test_sum_square_numbers_3(self):
+        self.assertEqual(sum_square_numbers(3), 5)
+    
+    def test_sum_square_numbers_4(self):
+        self.assertEqual(sum_square_numbers(4), 14)
