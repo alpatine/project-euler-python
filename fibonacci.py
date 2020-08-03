91ad861cc4e53e2af0e3c1a9f7ee46196822356c
Alister McKinley <alpatine@gmail.com>
2020-08-03 23:04:13 +1000

    Document helper functions

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,11 @@
 from typing import Iterator
 
 def fibonacci(upper: int) -> Iterator[int]:
+    """Generate the fibonacci sequence of numbers.
+
+    The sequence outputs 1 once e.g. 1, 2, 3, 5, ...
+    The sequence stops with the highest value less than upper.
+    """
     a, b = 1, 2
     while a < upper:
         yield a
--- primes.py
+++ primes.py
@@ -3,6 +3,11 @@ from itertools import islice
 from typing import Iterator, Set
 
 def primes(upper: int) -> Iterator[int]:
+    """Generate the prime numbers.
+
+    The sequence starts with 2 and continues until upper is reached.
+    The value upper is not included in the sequence.
+    """
     prime_list = [True] * upper
     max_factor = floor(sqrt(upper))
 
@@ -14,6 +19,11 @@ def primes(upper: int) -> Iterator[int]:
                     prime_list[j] = False
 
 def prime_factors(number: int) -> Set[int]:
+    """Calculate the primes that will divide number.
+
+    Prime numbers with a power of zero are not included.
+    Prime numbers with a power greater than 1 are not repeated.
+    """
     result = set()
     current_factor = 2
     while number > 1:
@@ -26,10 +36,16 @@ def prime_factors(number: int) -> Set[int]:
     return result
 
 def estimate_nth_prime(n: int) -> int:
+    """Estimate the n'th prime number.
+
+    Estimation is based on n * ln(n) because the primes get closer to
+    this value as n gets larger.
+    """
     # As n gets larger, the nth prime ~ n * ln(n)
     return int(n * log(n))
 
 def nth_prime(n: int) -> int:
+    """Calculate the n'th prime number."""
     if n == 1: return 2
     if n == 2: return 3
     estimate = estimate_nth_prime(n)
