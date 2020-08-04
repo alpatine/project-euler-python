9accaf0c91830198ee19df23666997fd912b0700
Alister McKinley <alpatine@gmail.com>
2020-08-04 23:31:59 +1000

    Format and document Pythagoras functions

--- pythagoras.py
+++ pythagoras.py
@@ -1,19 +1,12 @@
 from typing import Tuple
 
 def calculate_next_triplets(triplet: Tuple[int, int, int]) -> Tuple[Tuple[int, int, int],
-                                                                  Tuple[int, int, int],
-                                                                  Tuple[int, int, int]]:
+                                                                    Tuple[int, int, int],
+                                                                    Tuple[int, int, int]]:
+    """Calculate pythagorean triplets from an initial triplet."""
     a, b, c = triplet
-    A = (a - 2 * b + 2 * c,
-         2 * a - b + 2 * c,
-         2 * a - 2 * b + 3 * c)
-    
-    B = (a + 2 * b + 2 * c,
-         2 * a + b + 2 * c,
-         2 * a + 2 * b + 3 * c)
-
-    C = (-a + 2 * b + 2 * c,
-         -2 * a + b + 2 * c,
-         -2 * a + 2 * b + 3 * c)
+    A = (a - 2 * b + 2 * c, 2 * a - b + 2 * c, 2 * a - 2 * b + 3 * c)
+    B = (a + 2 * b + 2 * c, 2 * a + b + 2 * c, 2 * a + 2 * b + 3 * c)
+    C = (-a + 2 * b + 2 * c, -2 * a + b + 2 * c, -2 * a + 2 * b + 3 * c)
 
     return (A, B, C)
