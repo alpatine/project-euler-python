75e76a2f676ae93b63ed33e2eb0438bf690999e9
Alister McKinley <alpatine@gmail.com>
2021-12-30 15:09:14 +1100

    Implement problem 41

---
+++ p41.py
@@ -0,0 +1,12 @@
+from number_theory import primes
+from digits import is_pandigital
+
+def p41():
+    primes_list = [p for p in primes(10000000) if len(str(p)) in [4, 7]]
+    primes_list.reverse()
+    return next(pandigital_prime
+                for pandigital_prime in primes_list
+                if is_pandigital(str(pandigital_prime), len(str(pandigital_prime))))
+
+if __name__ == '__main__':
+    print(p41())
