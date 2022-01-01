90516d663b7ea79ef809290f0c5d4398f0b7fb4f
Alister McKinley <alpatine@gmail.com>
2022-01-01 13:37:35 +1100

    Correct comments in sum functions

--- sums.py
+++ sums.py
@@ -1,9 +1,9 @@
 def sum_numbers(upper: int) -> int:
-    """Calculate the sum of integers less than upper."""
+    """Calculate the sum of naturals less than upper."""
     n = upper - 1
     return n * (n + 1) // 2
 
 def sum_square_numbers(upper: int) -> int:
-    """Calculate the sum of the squares of the integers less than upper."""
+    """Calculate the sum of the squares of the naturals less than upper."""
     n = upper - 1
     return n * (2 * n + 1) * (n + 1) // 6
