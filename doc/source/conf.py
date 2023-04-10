d1eec6de3855622eca3c1daabdca5ead98a6c8d6
Alister McKinley <alpatine@gmail.com>
2023-04-10 23:43:32 +1000

    Quickstart sphinx, apply theme, add 2 pages

---
+++ doc/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'The Project Euler 100'
+copyright = '2023, Alister McKinley'
+author = 'Alister McKinley'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_title = 'The Project Euler 100'
+html_static_path = ['_static']
---
+++ doc/source/index.rst
@@ -0,0 +1,17 @@
+.. Project Euler - The First 100 Problems documentation master file, created by
+   sphinx-quickstart on Mon Apr 10 20:22:19 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to The Project Euler 100
+==================================================================
+
+This site is dedicated to providing solutions to the first 100 problems
+published on the Project Euler website.
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   prelim/sum-of-square-numbers
+   prelim/fibonacci-formula
