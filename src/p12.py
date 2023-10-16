751c99239dd8c54aebd034cd97522fabb404e8c0
Alister McKinley <alpatine@gmail.com>
2023-10-16 23:46:15 +1100

    Simplify solution to problem 12

--- src/p12.py
+++ src/p12.py
@@ -2,18 +2,11 @@ from number_theory import count_divisors, primes
 
 def p12(divisor_target: int) -> int:
     prime_number_list = list(primes(1000))
-    previous_factor_divisors = 1
     for n in range (1, 2 ** 30):
-        current_factor = n + 1
-        if current_factor % 2 == 0:
-            current_factor //= 2
-        
-        current_factor_divisors = count_divisors(current_factor, prime_number_list)
-        divisor_count = previous_factor_divisors * current_factor_divisors
-        if divisor_count >= divisor_target:
-            return n * (n + 1) // 2
-
-        previous_factor_divisors = current_factor_divisors
+        triangle_number = n * (n + 1) // 2
+        number_of_divisors = count_divisors(triangle_number, prime_number_list)
+        if number_of_divisors >= divisor_target:
+            return triangle_number
 
 if __name__ == '__main__':
     print(p12(5))
