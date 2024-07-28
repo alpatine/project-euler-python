383a4c64f4b01505d12c7170c4a9d633fa67ec36
Alister McKinley <alpatine@gmail.com>
2024-07-28 21:40:47 +1000

    Make p14 start checking from 1 instead of half the stop value

--- src/p14.py
+++ src/p14.py
@@ -4,7 +4,7 @@ def p14(stop: int) -> int:
     number_with_max_collatz_length = 0
     max_collatz_length = 0
     if stop == 1: return 1
-    for n in range(stop//2, stop):
+    for n in range(1, stop):
         length = collatz_length(n)
         if length > max_collatz_length:
             max_collatz_length = length
